e73d8d80e86854c31a6549832366d5447b88d052
Kevin Alavik <kevin@alavik.se>
2025-04-14 13:15:54 +0200

    fix: Reading FAT12 header from boot sector in stage2

--- boot/loader.asm
+++ boot/loader.asm
@@ -7,16 +7,42 @@ BITS    16
 
 ; === PiratDOS V1.0 Bootloader entry point ===
 ENTRY:
+    ; Disable cursor using dirty hardcoded value
+    MOV AH, 0001h
+    MOV CX, 2607h
+    INT 10h
+
+    ; Render the primary UI
     CALL RNDR_UI
-    LEA SI, [NOTE]
-    CALL PRINTZ
-.HALT:
-    HLT
-    JMP $
+
+    ; Load FAT12 header from boot sector
+    MOV AX, 0                       ; LBA = 0 (Boot sector)
+    MOV CL, 1                       ; Read a single sector
+    LEA BX, [READ_BUFF]             ; The buffer to store the sector
+    CALL DISK_READ
+    JC DISK_READ_ERROR
+
+    ; Print out VOLUME_LABEL
+    LEA SI, [READ_BUFF + 2Bh]       ; READ_BUFF + Offset for VOLUME_LABEL in FAT12 header
+    MOV AX, 11                      ; Length of VOLUME_LABEL section
+    CALL PRINTN
+
+    ; Halt the system, HALT is defined in error.asm
+    JMP HALT
 
 ; === Includes ===
 %INCLUDE "util/render.asm"
 %INCLUDE "util/ui.asm"
+%INCLUDE "util/error.asm"
+%INCLUDE "sys/disk.asm"
 
 ; === Data ===
 NOTE: DB 'No bootable options available currently.', 0Ah, 0Dh, 0
+
+; Needed for DISK_READ
+SECTORS_PER_TRACK      EQU 18  ; Sectors per track from the boot sector
+HEADS                  EQU 2   ; Number of heads from the boot sector
+DISK_READ_RETRY_COUNT  EQU 10  ; Number of retries for disk read
+
+; Read buffers
+READ_BUFF:
